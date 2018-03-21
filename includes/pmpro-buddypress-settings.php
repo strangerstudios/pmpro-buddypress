a423d6b29a51fca4bb2f66f1114d797626ebfb4f
kimcoleman <kim@strangerstudios.com>
2018-03-21 16:29:52 -0400

    Improving settings page help text and layout; adding admin header and footer

--- includes/pmpro-buddypress-settings.php
+++ includes/pmpro-buddypress-settings.php
@@ -35,7 +35,7 @@ add_filter('pmpro_register_redirect', 'pmpro_bp_registration_pmpro_to_bp_redirec
 
 function pmpro_bp_buddpress_admin_page()
 {
-		//get/set settings
+	//get/set settings
 	if(!empty($_REQUEST['savesettings']))
 	{
 		update_option('pmpro_bp_registration_page', $_POST['pmpro_bp_register']);
@@ -49,41 +49,65 @@ function pmpro_bp_buddpress_admin_page()
 		$pmpro_bp_register = 'pmpro'; //default to the PMPro Levels page 
     
 	if(empty($pmpro_bp_level_profile))
-		$pmpro_bp_level_profile = 'yes'; //default to showing Level on BuddyPress Profile ?>
+		$pmpro_bp_level_profile = 'yes'; //default to showing Level on BuddyPress Profile 
 
-	<h2>PMPro BuddyPress Settings</h2>
+	require_once( PMPRO_DIR . '/adminpages/admin_header.php' ); ?>
+
+	<div id="poststuff">
+		<h1><?php esc_attr_e( 'Paid Memberships Pro - BuddyPress Add On Settings', 'pmpro-buddypress' ); ?></h1>
+		<p><?php printf( __( 'Integrate and manage your BuddyPress Community using Paid Memberships Pro. <a href="%s" target="_blank">Read the documentation</a> for more information about this Add On.', 'pmpro-buddypress' ), 'https://www.paidmembershipspro.com/add-ons/pmpro-buddypress/' ); ?></p>
+
+		<h3 class="topborder"><?php esc_attr_e( 'Page Settings', 'pmpro-buddypress' ); ?></h3>
+		<p><?php esc_attr_e( 'This plugin redirects users to a specific page if they try to access restricted BuddyPress features. The user is redirected to the page assigned as the "Access Restricted" page under Memberships > Page Settings.', 'pmpro-buddypress' ); ?></p>
+		<?php
+			global $pmpro_pages;
+			$pmprobp_restricted_page = $pmpro_pages['pmprobp_restricted'];
+			if ( ! empty( $pmprobp_restricted_page ) ) {
+				$msgt = '<span class="dashicons dashicons-yes"></span>' . esc_attr( '"Access Restricted" page is configured.', 'pmpro-buddypress' );
+				$msgc = '#46b450';
+			} else {
+				$msgt = '<span class="dashicons dashicons-no"></span>' . esc_attr( '"Access Restricted" page is not configured.', 'pmpro-buddypress' );
+				$msgc = '#a00';
+			}
+		?>
+		<p><strong style="color: <?php echo $msgc; ?>"><?php echo $msgt; ?></strong></p>
+		<p><a href="<?php echo admin_url('admin.php?page=pmpro-pagesettings');?>" class="button button-primary"><?php _e('Manage Page Settings', 'paid-memberships-pro' );?></a></p>
+
+		<h3 class="topborder"><?php esc_attr_e( 'Membership Level Settings', 'pmpro-buddypress' ); ?></h3>
+		<p><?php esc_attr_e( 'Edit your membership levels to set level-specific restrictions on BuddyPress features. Features that can be restricted include: Group Creation, Single Group Viewing, Groups Page Viewing, Joining Groups, Public Messaging, Private Messaging, Send Friend Requests, and inclusion in the Member Directory.', 'pmpro-buddypress' ); ?></p>
+		<p><a href="<?php echo admin_url('admin.php?page=pmpro-membershiplevels');?>" class="button button-primary"><?php _e('Edit Membership Levels', 'paid-memberships-pro' );?></a></p>
 		<form action="" method="post" enctype="multipart/form-data">
-		
-		<table class="form-table">
-		<tbody>
-			<tr>
-				<th scope="row" valign="top">
-					<label for="pmpro_bp_register"><?php _e("Registration Page", 'pmpro');?></label>
-				</th>
-				<td>
-					<select id="pmpro_bp_register" name="pmpro_bp_register">
-						<option value="pmpro" <?php if($pmpro_bp_register == 'pmpro') { ?>selected="selected"<?php } ?>><?php _e('Use PMPro Levels Page', 'pmpro');?></option>
-						<option value="buddypress" <?php if($pmpro_bp_register == 'buddypress') { ?>selected="selected"<?php } ?>><?php _e('Use BuddyPress Registration Page', 'pmpro');?></option>
-					</select>
-				</td>
-			</tr>
-			
-			<tr>
-				<th scope="row" valign="top">
-					<label for="pmpro_bp_level_profile"><?php _e("Show Membership Level on BuddyPress Profile?", 'pmpro');?></label>
-				</th>
-				<td>
-					<select id="pmpro_bp_level_profile" name="pmpro_bp_level_profile">
-						<option value="yes" <?php if($pmpro_bp_level_profile == 'yes') { ?>selected="selected"<?php } ?>><?php _e('Yes', 'pmpro');?></option>
-						<option value="no" <?php if($pmpro_bp_level_profile == 'no') { ?>selected="selected"<?php } ?>><?php _e('No', 'pmpro');?></option>
-					</select>
-				</td>
-			</tr>
-	</tbody>
-	</table>
-
-	<p class="submit">
-		<input name="savesettings" type="submit" class="button button-primary" value="<?php _e('Save Settings', 'pmpro');?>" />
-	</p>
-</form><?php
+			<h3 class="topborder"><?php esc_attr_e( 'General Settings', 'pmpro-buddypress' ); ?></h3>
+			<table class="form-table">
+			<tbody>
+				<tr>
+					<th scope="row" valign="top">
+						<label for="pmpro_bp_register"><?php _e("Registration Page", 'pmpro');?></label>
+					</th>
+					<td>
+						<select id="pmpro_bp_register" name="pmpro_bp_register">
+							<option value="pmpro" <?php if($pmpro_bp_register == 'pmpro') { ?>selected="selected"<?php } ?>><?php _e('Use PMPro Levels Page', 'pmpro');?></option>
+							<option value="buddypress" <?php if($pmpro_bp_register == 'buddypress') { ?>selected="selected"<?php } ?>><?php _e('Use BuddyPress Registration Page', 'pmpro');?></option>
+						</select>
+					</td>
+				</tr>
+				
+				<tr>
+					<th scope="row" valign="top">
+						<label for="pmpro_bp_level_profile"><?php _e("Show Membership Level on BuddyPress Profile?", 'pmpro');?></label>
+					</th>
+					<td>
+						<select id="pmpro_bp_level_profile" name="pmpro_bp_level_profile">
+							<option value="yes" <?php if($pmpro_bp_level_profile == 'yes') { ?>selected="selected"<?php } ?>><?php _e('Yes', 'pmpro');?></option>
+							<option value="no" <?php if($pmpro_bp_level_profile == 'no') { ?>selected="selected"<?php } ?>><?php _e('No', 'pmpro');?></option>
+						</select>
+					</td>
+				</tr>
+			</tbody>
+			</table>
+			<p class="submit">
+				<input name="savesettings" type="submit" class="button button-primary" value="<?php _e('Save Settings', 'pmpro');?>" />
+			</p>
+		</form>
+<?php
 }
