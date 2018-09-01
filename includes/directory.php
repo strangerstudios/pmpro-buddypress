d0e41d81b3db61d53b55992cd6a9773d983a2783
Paul Barthmaier <paul@barthmaier.rocks>
2018-09-01 11:30:54 -0400

    Fixed SQL syntax
    WordPress database error You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near ') AND status = 'active'' at line 1 for query SELECT DISTINCT user_id FROM wp_pmpro_memberships_users WHERE membership_id IN () AND status = 'active' made by require_once('wp-admin/admin.php'), require_once('wp-load.php'), require_once('wp-config.php'), require_once('wp-settings.php'), do_action('init'), WP_Hook->do_action, WP_Hook->apply_filters, pmpro_bp_directory_init, pmpro_bp_get_members_in_directory

--- includes/directory.php
+++ includes/directory.php
@@ -5,7 +5,7 @@
 
 function pmpro_bp_directory_init() {
 	// Don't do this if PMPro is deactivated
-	if( !defined( 'PMPRO_VERSION' ) ) {
+	if ( ! defined( 'PMPRO_VERSION' ) ) {
 		return;
 	}
 
@@ -14,65 +14,74 @@ function pmpro_bp_directory_init() {
 	add_action( 'bp_pre_user_query_construct', 'pmpro_bp_bp_pre_user_query_construct', 1, 1 );
 	add_filter( 'bp_get_total_member_count', 'pmpro_bp_bp_get_total_member_count' );
 }
-add_action('init', 'pmpro_bp_directory_init', 20);
+add_action( 'init', 'pmpro_bp_directory_init', 20 );
 
 function pmpro_bp_bp_pre_user_query_construct( $query_array ) {
 	// If no setting is locking the member directory, let's bail.
-	if( !pmpro_bp_is_member_directory_locked() ) {
+	if ( ! pmpro_bp_is_member_directory_locked() ) {
 		return;
 	}
 
 	global $pmpro_bp_members_in_directory;
-	if( !empty( $pmpro_bp_members_in_directory ) ) {
+	if ( ! empty( $pmpro_bp_members_in_directory ) ) {
 		// If an include value was already set, make sure it's in array form.
-		if( !empty( $query_array->query_vars['include'] ) && !is_array( $query_array->query_vars['include']) ) {
+		if ( ! empty( $query_array->query_vars['include'] ) && ! is_array( $query_array->query_vars['include'] ) ) {
 			$query_array->query_vars['include'] = explode( ',', $query_array->query_vars['include'] );
 		}
 
-		if( is_array( $query_array->query_vars['include'] ) ) {
+		if ( is_array( $query_array->query_vars['include'] ) ) {
 			// Compute the intersect of members and include value.
-			$query_array->query_vars['include'] = array_intersect( $query_array->query_vars['include'], $pmpro_bp_members_in_directory );	
+			$query_array->query_vars['include'] = array_intersect( $query_array->query_vars['include'], $pmpro_bp_members_in_directory );
 		} else {
 			// Only include members in the directory.
 			$query_array->query_vars['include'] = $pmpro_bp_members_in_directory;
 		}
 	} else {
 		// No members, block the directory.
-		$query_array->query_vars['include'] = array(0);
+		$query_array->query_vars['include'] = array( 0 );
 	}
 }
 
-function pmpro_bp_bp_get_total_member_count($count) {
+function pmpro_bp_bp_get_total_member_count( $count ) {
 	global $pmpro_bp_members_in_directory;
 
-	$count = count($pmpro_bp_members_in_directory);
+	$count = count( $pmpro_bp_members_in_directory );
 	return $count;
 }
 
 function pmpro_bp_get_members_in_directory() {
 	global $wpdb, $pmpro_levels;
 
-	if( !function_exists( 'pmpro_getAllLevels' ) ) {
+	if ( ! function_exists( 'pmpro_getAllLevels' ) ) {
 		return array();
 	}
 
-	$pmpro_levels = pmpro_getAllLevels(false, true);
+	$pmpro_levels = pmpro_getAllLevels( false, true );
 
-	//see if we should include them in the member directory.
+	// see if we should include them in the member directory.
 	$include_levels = array();
 
-	foreach($pmpro_levels as $level) {
+	foreach ( $pmpro_levels as $level ) {
 		$pmpro_bp_options = pmpro_bp_get_level_options( $level->id );
 
-		if( $pmpro_bp_options['pmpro_bp_member_directory'] == 1 || $pmpro_bp_options['pmpro_bp_restrictions'] == 1) {
+		if ( $pmpro_bp_options['pmpro_bp_member_directory'] == 1 || $pmpro_bp_options['pmpro_bp_restrictions'] == 1 ) {
 			$include_levels[] = $level->id;
 		}
 	}
-
-	$sql = "SELECT DISTINCT user_id FROM $wpdb->pmpro_memberships_users WHERE membership_id IN (" . implode(",", array_map("intval", $include_levels)) . ") AND status = 'active'";
+	$mapped_levels = implode( ',', array_map( 'intval', $include_levels ) );
+	$sqlQuery = $wpdb->prepare(
+		"
+		SELECT DISTINCT user_id 
+		FROM $wpdb->pmpro_memberships_users 
+		WHERE membership_id 
+		IN(%s) 
+		AND status = 'active'
+		",
+		$mapped_levels
+	);
 
 	$wpdb->flush();
-	$include_users = $wpdb->get_col($sql);
+	$include_users = $wpdb->get_col( $sqlQuery );
 
 	return $include_users;
 }
@@ -80,7 +89,7 @@ function pmpro_bp_get_members_in_directory() {
 function pmpro_bp_is_member_directory_locked() {
 	$non_user_options = pmpro_bp_get_level_options( 0 );
 
-	if( !$non_user_options['pmpro_bp_member_directory'] ) {
+	if ( ! $non_user_options['pmpro_bp_member_directory'] ) {
 		return true;
 	} else {
 		return false;
