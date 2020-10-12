2eede85c556125b5f5051e91575bd8a4706cfd23
Jason Coleman <jason@strangerstudios.com>
2020-10-13 04:56:03 +1100

    Now removing the create group cap to prevent users without access from creating groups. We used to just disable the button. Now the button won't show. And visiting the URL directly won't work either.

--- includes/restrictions.php
+++ includes/restrictions.php
@@ -52,11 +52,25 @@ function pmpro_bp_bp_get_group_create_button( $button_args ) {
 	
 	if(!pmpro_bp_user_can( 'pmpro_bp_group_creation' ) ) {
 		$button_args['link_href'] =	get_permalink($pmpro_pages['pmprobp_restricted']);
+		$button_args['button_element'] = 'a';
 	}
 	
     return $button_args;
 }         
 add_filter( 'bp_get_group_create_button', 'pmpro_bp_bp_get_group_create_button', 10, 1 );
+
+/**
+ * Prevent users from creating groups
+ * if their level doesn't allow it.
+ */
+function pmpro_bp_bp_user_can_create_groups( $can_create, $restricted ) {
+	if ( $can_create && ! pmpro_bp_user_can( 'pmpro_bp_group_creation' ) && ! current_user_can( 'manage_options' ) ) {
+		$can_create = false;
+	}
+	
+	return $can_create;
+}
+add_filter( 'bp_user_can_create_groups', 'pmpro_bp_bp_user_can_create_groups', 10, 2);
 
 /**
  * Hide the Join Group button if joining groups is restricted
